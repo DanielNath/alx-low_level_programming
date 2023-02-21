4f08a8ebc193779b2ac48498cc2ee9c8e78001f6
danndon <you@example.com>
2023-02-20 19:15:42 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that print text using puts fuction
+ * Return: 0 (sucess)
+ */
+int main(void)
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
