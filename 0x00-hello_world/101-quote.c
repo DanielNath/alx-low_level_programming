564ce38c51c8c11d93996198cd261ba4c08e65ff
danndon <you@example.com>
2023-02-21 15:26:44 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
-* main - A programm that prints stard erro using write function
+* main - programe to print standard erro
 *
 * Return: 1 (success)
 */
 int main(void)
-}
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
