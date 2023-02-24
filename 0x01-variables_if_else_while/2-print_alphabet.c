80c97101e4e28f916970bf430541318e07f63fe7
danndon <you@example.com>
2023-02-24 02:27:57 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,9 @@ int main(void)
 	char alp[26] = "abcdefghijklmnopqrstuvuxyz";
 	int i;
 
-	for (i = 0; i < 26; i++);
-	{
+	for (i = 0; i < 26; i++)
 	putchar(alp[i]);
-	}
+
 	putchar('\n');
 	return (0);
 }
