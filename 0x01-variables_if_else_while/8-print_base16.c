3c70ae538e0072a069ff1ec62a89fe612dad5cf3
danndon <you@example.com>
2023-02-28 03:45:25 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all single digit numbers of base 10 from 0
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+	putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
