522eb5ba4800a450666db4f3fe2e4a74bbc08ac4
danndon <you@example.com>
2023-03-05 15:00:10 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+
+void print_alphabet(void);
+{
+	char lcleta;
+
+	for (lcleta = 'a'; lcleta <= 'z'; lcleta++)
+	_putchar(lcleta);
+
+	_putchar('\n');
+}
