cde456ed134da4aa9ffae70644d42836793d6237
danndon <you@example.com>
2023-03-05 15:05:04 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,10 @@
 
 void print_alphabet(void);
 {
-	char lcleta;
+	char letter;
 
-	for (lcleta = 'a'; lcleta <= 'z'; lcleta++)
-	_putchar(lcleta);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
 
 	_putchar('\n');
 }
