0f173e7095ebdc091993835c885ae341b7585623
danndon <you@example.com>
2023-03-05 13:37:42 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - a program that prints _putchar
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char cha[7] = "putchar";
+	int i;
+
+	{
+		putchar('_');
+	}
+	for (i = 0; i < 8; i++)
+	{
+		putchar(cha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
