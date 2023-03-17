0d4ee2cf40c936b62bd6c3830ff616f329f67f40
danndon <you@example.com>
2023-03-17 13:44:57 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	ptstr = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
+	if (size == 0 || ptstr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		str[i] = c;
+		ptstr[i] = c;
 	return (ptstr);
 }
